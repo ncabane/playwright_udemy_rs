8815297575f5e194f2655134929241b7821e24f9
ncabane <ncabane@gmail.com>
2023-07-25 22:40:05 +0200

    ok but wip

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,7 +1,7 @@
 const {test,expect} = require('@playwright/test')
 
 
-test.only("Popup validations", async({page}) =>
+test("Popup validations", async({page}) =>
 {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goto("https://google.com");
